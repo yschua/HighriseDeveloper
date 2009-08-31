f723d54999231580159873965b04cf1b9c4718d1
alabamacajun <alabamacajun@f146f8d7-247d-460d-a1b3-d0a612191574>
2009-08-31 15:42:32 +0000

    Pulled LOKI for now until we can make more use of it. Too much library for one function object. It is a nice library. Using FunkyT which needs to be implemented.
    git-svn-id: https://svn.code.sf.net/p/highrisedev/code@48 f146f8d7-247d-460d-a1b3-d0a612191574

--- Window/Element.h
+++ Window/Element.h
@@ -1,18 +1,21 @@
 #pragma once
 
 #include <SFML/Graphics.hpp>
-#include <loki/Functor.h>
+//#include <loki/Functor.h>  // AC pulled LOKI for now
 //#include "../Graphics/Graphics.h"
 #include "../highrisedev.h"
 
 //#include "Window.h"
 
-namespace hr {
+namespace hr
+{
     class Window;
-    namespace UI {
+    namespace UI
+    {
         /// This class is used exclusivly for changing the style of UI
         /// elements in the different states.
-        class ElementStyle {
+        class ElementStyle
+        {
             public:
             ElementStyle();
             sf::Color ActiveColor;
@@ -24,7 +27,25 @@ namespace hr {
         /// create instances of a derived class instead. All UI elements
         /// must inheriet from this base class.
         //////////////////////////////////////////////////////////////////
-        class Element {
+
+        template <typename T> class FunkyT  // the Functor
+        {
+           T* mT;
+        public:
+           FunkyT()
+           {
+              mT = NULL;
+              // TODO: nothing happening here crazy, car to implement
+           }
+           FunkyT(T* pT)
+              : mT(pT)
+           {
+              // TODO: nothing happening here crazy, car to implement
+           }
+        };
+
+        class Element
+        {
             public:
             Element();
             virtual void Draw()=0;
@@ -38,7 +59,8 @@ namespace hr {
             virtual bool Contains(Vector2i Mouse)=0;
             virtual void MouseIn()=0;
             virtual void MouseOut()=0;
-            typedef Loki::Functor<void, LOKI_TYPELIST_1(Element*)> ActionType;
+//            typedef Loki::Functor<void, LOKI_TYPELIST_1(Element*)> ActionType;
+            typedef FunkyT<Element*> ActionType;
             void SetAction(std::string Key, ActionType Action);
             //friend class OT::Window;
             //friend class EventMgr;

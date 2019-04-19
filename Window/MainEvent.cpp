ba8c39e47b79b0f20ec305ff820986ee3352d47c
yschua <4900895+yschua@users.noreply.github.com>
2019-04-19 13:15:15 +1000

    Add license info

--- Window/MainEvent.cpp
+++ Window/MainEvent.cpp
@@ -1,3 +1,19 @@
+/*   This file is part of Highrise Developer.
+*
+*   Highrise Developer is free software: you can redistribute it and/or modify
+*   it under the terms of the GNU General Public License as published by
+*   the Free Software Foundation, either version 3 of the License, or
+*   (at your option) any later version.
+
+*   Highrise Developer is distributed in the hope that it will be useful,
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*   GNU General Public License for more details.
+*
+*   You should have received a copy of the GNU General Public License
+*   along with Highrise Developer.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include "MainEvent.h"
 
 const sf::Time MainEvent::m_dtNormal = sf::milliseconds(10);

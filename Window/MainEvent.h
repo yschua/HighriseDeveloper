679c326426b13d2c9e95f4dac6d3cc6acb199809
yschua <4900895+yschua@users.noreply.github.com>
2019-04-19 13:15:15 +1000

    Add license info

--- Window/MainEvent.h
+++ Window/MainEvent.h
@@ -1,3 +1,19 @@
+/*   This file is part of Highrise Developer.
+*
+*   Highrise Developer is free software: you can redistribute it and/or modify
+*   it under the terms of the GNU General Public License as published by
+*   the Free Software Foundation, either version 3 of the License, or
+*   (at your option) any later version.
+
+*   Highrise Developer is distributed in the hope that it will be useful,
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*   GNU General Public License for more details.
+*
+*   You should have received a copy of the GNU General Public License
+*   along with Highrise Developer.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #ifndef _MAINEVENT_H
 #define _MAINEVENT_H
 
@@ -14,10 +30,9 @@ public:
     bool IsPaused() const;
     sf::Time GetDt() const;
 
+private:
     static const sf::Time m_dtNormal;
     static const sf::Time m_dtFast;
-
-private:
     sf::Time m_dt; // timestep
     bool m_closed;
     bool m_paused;
